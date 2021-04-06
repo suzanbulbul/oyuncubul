b78f69e4fe686a9609f174981364c3acd0cb8c3f
Suzan Bülbül <63511980+suzanbulbul@users.noreply.github.com>
2021-04-06 15:21:34 +0300

    login ve sign up sayfalarının geliştirilmesi
    login ve sign up sayfalarının temel seviyede geliştirilmesi

---
+++ lib/SignUp/sign_up_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:my_fit/screens/SignUp/components/body.dart';
+
+class SignupScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
